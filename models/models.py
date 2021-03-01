9c75a14612c9764c0aba59680c07788916c43380
Your Name <you@example.com>
2021-02-28 22:55:37 -0300

    Auto refresh

--- models/models.py
+++ models/models.py
@@ -8,7 +8,7 @@ class PosOrderLine(models.Model):
 
     categ_id = fields.Many2one(string='Categoría del Producto',related="product_id.product_tmpl_id.pos_categ_id")
     table_id = fields.Many2one(string='Mesa', related="order_id.table_id")
-    
+    date_order = fields.Datetime(string='Fecha' , related="order_id.date_order")
     
     
     
