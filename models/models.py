13285fc17e1de2f4e6dee03bf10fff0e9874cd43
Your Name <you@example.com>
2021-02-28 22:51:04 -0300

    Auto refresh

--- models/models.py
+++ models/models.py
@@ -7,6 +7,8 @@ class PosOrderLine(models.Model):
     _inherit = 'pos.order.line'
 
     categ_id = fields.Many2one(string='Categoría del Producto',related="product_id.product_tmpl_id.pos_categ_id")
+    table_id = fields.Many2one(string='Mesa', related="order_id.table_id")
+    
     
     
     
